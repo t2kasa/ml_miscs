88dc46ba8e4bdc4455a7b8792ab4f631e5f0c6a4
Tsukasa Takagi <takagi.tsukasa0@gmail.com>
2018-12-30 00:19:30 +0900

    works

--- examples/gmm_old_faithful.py
+++ examples/gmm_old_faithful.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 from matplotlib.patches import Ellipse
 from scipy.stats import chi2
-
+from matplotlib.colors import BASE_COLORS
 from gmm import *
 
 
@@ -28,12 +28,11 @@ def _eig_sort(a):
     return values, vectors
 
 
-def plot_gmm_confidence_ellipses(mean, cov, ellipse_colors, confidence=0.95,
+def plot_gmm_confidence_ellipses(ax, mean, cov, colors, confidence=0.95,
                                  plot_eigenvectors=True):
     n_components = len(mean)
     alpha = np.sqrt(chi2(2).ppf(confidence))
 
-    fig, ax = plt.subplots(1, 1, figsize=(8, 8))
     ax.set_xlim(-2.0, 2.0)
     ax.set_ylim(-2.0, 2.0)
 
@@ -41,39 +40,54 @@ def plot_gmm_confidence_ellipses(mean, cov, ellipse_colors, confidence=0.95,
         # plot ellipse from covariance
         values, vectors = _eig_sort(cov[k])
         w, h = 2 * alpha * np.sqrt(values)
-        angle = np.degrees(np.arctan2(vectors[0, 1], vectors[0, 0]))
+        angle = np.degrees(np.arctan2(vectors[1, 0], vectors[0, 0]))
         ax.add_artist(
-            Ellipse(mean[k], w, h, angle, color=ellipse_colors[k], fill=False))
+            Ellipse(mean[k], w, h, angle, color=colors[k], fill=False))
 
         # plot eigenvectors if needed
         if plot_eigenvectors:
-            eigen_lines = [np.stack([mean[k], mean[k] + vectors[:, 0] * w / 2]),
-                           np.stack([mean[k], mean[k] + vectors[:, 1] * h / 2])]
-            for line in eigen_lines:
-                ax.plot(line[:, 0], line[:, 1], 'black')
+            arrow_params = {'color': colors[k], 'length_includes_head': True,
+                            'head_width': 0.05, 'head_length': 0.1}
 
-    return fig, ax
+            ax.arrow(*mean[k], *(vectors[:, 0] * w / 2), **arrow_params)
+            ax.arrow(*mean[k], *(vectors[:, 1] * h / 2), **arrow_params)
+
+            # eigen_lines = [np.stack([mean[k], mean[k] + vectors[:, 0] * w / 2]),
+            #                np.stack([mean[k], mean[k] + vectors[:, 1] * h / 2])]
+            # for line in eigen_lines:
+            #     ax.arrow(*mean, color=colors[k])
+            #     # ax.plot(line[:, 0], line[:, 1], colors[k])
+            #
+
+def save_history_as_video_file(history, save_video_file):
+    fig, ax = plt.subplots(1, 1, figsize=(8, 8))
+
+    def animate(t):
+        nonlocal history, ax
+        ax.clear()
+        plot_gmm_confidence_ellipses(ax, history[t]['mean'], history[t]['cov'],
+                                     colors=list(BASE_COLORS.keys()))
+        return ax,
+
+    from matplotlib.animation import FFMpegWriter
+    writer = FFMpegWriter(fps=1)
+    with writer.saving(fig, save_video_file, dpi=100):
+        for t in range(len(history)):
+            animate(t)
+            writer.grab_frame()
 
 
 def main():
-    n_iters = 20
-    n_components = 2
+    n_components = 4
     x = get_data()
-
     gmm = GMM(n_components)
+
+    # gmm = GMM(n_components,
+    #           pi_init=np.ones(n_components) / n_components,
+    #           mean_init=np.stack([[0.0, 1.0], [0.0, -1.0]]),
+    #           cov_init=np.stack([0.1 * np.eye(2), 0.1 * np.eye(2)]))
     history = gmm.fit(x).history
-
-    # initialize model params
-    pi, mean, cov = init_params(x, n_components)
-
-    for _ in range(n_iters):
-        gamma = e_step_understandable(x, pi, mean, cov)
-        pi, mean, cov = m_step_understandable(x, gamma)
-        print(compute_log_likelihood(x, pi, mean, cov),
-              log_likelihood_understandable(x, pi, mean, cov))
-
-    fig, ax = plot_gmm_confidence_ellipses(mean, cov, ellipse_colors=('b', 'r'))
-    fig.show()
+    save_history_as_video_file(history, 'gmm.mp4')
 
 
 if __name__ == '__main__':
--- gmm.py
+++ gmm.py
@@ -5,13 +5,14 @@ from k_means import KMeans
 
 
 class GMM:
-    def __init__(self, n_components, max_iters=100, tol=1e-3):
+    def __init__(self, n_components, max_iters=100, tol=1e-3, pi_init=None,
+                 mean_init=None, cov_init=None):
         self.n_components = n_components
         self.max_iters = max_iters
         self.tol = tol
-        self.pi = None
-        self.mean = None
-        self.cov = None
+        self.pi = pi_init
+        self.mean = mean_init
+        self.cov = cov_init
         self.history = []
 
     def _init_params(self, x):
@@ -41,26 +42,28 @@ class GMM:
         return pi_init, mean_init, cov_init
 
     def fit(self, x):
-        self.pi, self.mean, self.cov = self._init_params(x)
-        self.history = {
-            'pi': [self.pi],
-            'mean': [self.mean],
-            'cov': [self.cov],
-            'log_likelihood': [
-                compute_log_likelihood(x, self.pi, self.mean, self.cov)]
-        }
+        # initialize params unless all params are not None.
+        if any([self.pi is None, self.mean is None, self.cov is None]):
+            self.pi, self.mean, self.cov = self._init_params(x)
+
+        self.history = [{
+            'pi': self.pi,
+            'mean': self.mean,
+            'cov': self.cov,
+            'log_likelihood': self._log_likelihood(x, self.pi, self.mean,
+                                                   self.cov)
+        }]
 
         prev_log_likelihood = None
         for i in range(self.max_iters):
-            gamma = e_step_understandable(x, self.pi, self.mean, self.cov)
-            self.pi, self.mean, self.cov = m_step_understandable(x, gamma)
-            log_likelihood = compute_log_likelihood(x, self.pi, self.mean,
-                                                    self.cov)
+            gamma = self._e_step(x, self.pi, self.mean, self.cov)
+            self.pi, self.mean, self.cov = self._m_step(x, gamma)
+            log_likelihood = self._log_likelihood(x, self.pi, self.mean,
+                                                  self.cov)
 
-            self.history['pi'].append(self.pi)
-            self.history['mean'].append(self.mean)
-            self.history['cov'].append(self.cov)
-            self.history['log_likelihood'].append(log_likelihood)
+            self.history.append(
+                {'pi': self.pi, 'mean': self.mean, 'cov': self.cov,
+                 'log_likelihood': log_likelihood})
 
             # convergence check
             if prev_log_likelihood is not None and \
@@ -71,163 +74,112 @@ class GMM:
 
         return self
 
+    @staticmethod
+    def _e_step(x, pi, mean, cov):
+        """Expectation Step.
 
-def init_params(x, n_components):
-    """Initialize GMM parameters by K-means.
+        :param x: (n_samples, n_features) features.
+        :param pi: (n_components,) mixing coefficients.
+        :param mean: (n_components, n_features) means.
+        :param cov: (n_components, n_features, n_features) covariances.
+        :return: (n_samples, n_components) responsibilities.
+        """
+        n_samples = len(x)
+        n_components = len(pi)
 
-    :param x: (n_samples, n_features) features.
-    :param n_components: the number of components.
-    :return: Initialized GMM parameters:
-        pi: (n_components,) mixing coefficients
-        mean: (n_components, n_features) means
-        cov: (n_components, n_features, n_features) covariances
-    """
-    n_samples, n_features = x.shape
+        gamma = np.zeros((n_samples, n_components))
+        for k in range(n_components):
+            prob_k = multivariate_normal.pdf(x, mean[k], cov[k])
+            gamma[:, k] = pi[k] * prob_k
 
-    k_means = KMeans(n_components)
-    assigned_indices = k_means.fit_predict(x)
-    mean_init = k_means.centers
+        gamma /= np.sum(gamma, axis=1, keepdims=True)
+        return gamma
 
-    pi_init = np.zeros(n_components)
-    cov_init = np.zeros((n_components, n_features, n_features))
-    for k in range(n_components):
-        cond = assigned_indices == k
-        d_k = x[cond] - mean_init[k]
-        pi_init[k] = np.sum(cond) / n_samples
-        cov_init[k] = np.dot(d_k.T, d_k) / np.sum(cond)
+    @staticmethod
+    def _m_step(x, gamma):
+        """Maximization step.
 
-    return pi_init, mean_init, cov_init
+        :param x: (n_samples, n_features) features.
+        :param gamma: (n_samples, n_components) responsibilities.
+        :return: Estimated GMM parameters.
+            pi: (n_components,) mixing coefficients
+            mean: (n_components, n_features) means
+            cov: (n_components, n_features, n_features) covariances
+        """
+        n_samples, n_features = x.shape
+        _, n_components = gamma.shape
 
+        n_effect = np.sum(gamma, axis=0)
+        pi = n_effect / n_samples
 
-def e_step(x, pi, mean, cov):
-    """Expectation Step.
+        mean = np.stack(
+            [np.sum(gamma[:, k:k + 1] * x, axis=0) / n_effect[k]
+             for k in range(n_components)])
 
-    :param x: (n_samples, n_features) features.
-    :param pi: (n_components,) mixing coefficients.
-    :param mean: (n_components, n_features) means.
-    :param cov: (n_components, n_features, n_features) covariances.
-    :return: (n_samples, n_components) responsibilities.
-    """
-    n_samples = len(x)
-    n_components = len(pi)
+        # covariance computation may be more simple.
+        cov = np.zeros((n_components, n_features, n_features))
+        for k in range(n_components):
+            d_k = x - mean[k]
+            cov[k] = np.dot(d_k.T, gamma[:, k:k + 1] * d_k) / n_effect[k]
 
-    gamma = np.zeros((n_samples, n_components))
-    for k in range(n_components):
-        prob_k = multivariate_normal.pdf(x, mean[k], cov[k])
-        gamma[:, k] = pi[k] * prob_k
+        return pi, mean, cov
 
-    gamma /= np.sum(gamma, axis=1, keepdims=True)
-    return gamma
+    @staticmethod
+    def _log_likelihood(x, pi, mean, cov):
+        n_samples = len(x)
+        n_components = len(pi)
 
+        pdfs = np.zeros((n_samples, n_components))
+        for k in range(n_components):
+            pdfs[:, k] = pi[k] * multivariate_normal.pdf(x, mean[k], cov[k])
 
-def e_step_understandable(x, pi, mean, cov):
-    """Expectation Step.
+        score = np.sum(np.log(np.sum(pdfs, axis=1)))
+        return score
 
-    :param x: (n_samples, n_features) features.
-    :param pi: (n_components,) mixing coefficients.
-    :param mean: (n_components, n_features) means.
-    :param cov: (n_components, n_features, n_features) covariances.
-    :return: (n_samples, n_components) responsibilities.
-    """
-    n_samples = len(x)
-    n_components = len(pi)
+    @staticmethod
+    def _e_step_understandable(x, pi, mean, cov):
+        n_samples = len(x)
+        n_components = len(pi)
 
-    gamma = np.zeros((n_samples, n_components))
-
-    for n in range(n_samples):
-        pdf_n = pi * np.array([multivariate_normal.pdf(x[n], mean_k, cov_k)
-                               for mean_k, cov_k in zip(mean, cov)])
-        gamma[n] = pdf_n / np.sum(pdf_n)
-    return gamma
-
-
-def m_step(x, gamma):
-    """Maximization step.
-
-    :param x: (n_samples, n_features) features.
-    :param gamma: (n_samples, n_components) responsibilities.
-    :return: Estimated GMM parameters.
-        pi: (n_components,) mixing coefficients
-        mean: (n_components, n_features) means
-        cov: (n_components, n_features, n_features) covariances
-    """
-    n_samples, n_features = x.shape
-    _, n_components = gamma.shape
-
-    n_effect = np.sum(gamma, axis=0)
-    pi = n_effect / n_samples
-    mean = np.dot(x.T, gamma) / n_effect
-
-    # covariance computation may be more simple.
-    cov = np.zeros((n_components, n_features, n_features))
-    for k in range(n_components):
-        d_k = x - mean[k]
-        cov[k] = np.dot(d_k.T, gamma[:, k:k + 1] * d_k) / n_effect[k]
-
-    return pi, mean, cov
-
-
-def m_step_understandable(x, gamma):
-    """Maximization step.
-
-    This function is understandable for GMM maximization step.
-
-    :param x:
-    :param gamma:
-    :return:
-    """
-    n_samples, n_features = x.shape
-    _, n_components = gamma.shape
-
-    # (9.27)
-    n_effect = np.zeros(n_components)
-    for k in range(n_components):
+        gamma = np.zeros((n_samples, n_components))
         for n in range(n_samples):
-            n_effect[k] += gamma[n, k]
+            pdf_n = pi * np.array(
+                [multivariate_normal.pdf(x[n], mean_k, cov_k)
+                 for mean_k, cov_k in zip(mean, cov)])
+            gamma[n] = pdf_n / np.sum(pdf_n)
+        return gamma
 
-    # (9.26)
-    pi = np.zeros(n_components)
-    for k in range(n_components):
-        pi[k] = n_effect[k] / n_samples
+    @staticmethod
+    def _m_step_understandable(x, gamma):
+        n_samples, n_features = x.shape
+        _, n_components = gamma.shape
 
-    # (9.24)
-    mean = np.zeros((n_components, n_features))
-    for k in range(n_components):
-        for n in range(n_samples):
-            mean[k] += gamma[n, k] * x[n]
-        mean[k] /= n_effect[k]
+        # (9.27)
+        n_effect = np.zeros(n_components)
+        for k in range(n_components):
+            for n in range(n_samples):
+                n_effect[k] += gamma[n, k]
 
-    # (9.25)
-    cov = np.zeros((n_components, n_features, n_features))
-    for k in range(n_components):
-        for n in range(n_samples):
-            cov[k] += gamma[n, k] * np.dot(
-                np.expand_dims(x[n] - mean[k], axis=-1),
-                np.expand_dims(x[n] - mean[k], axis=-1).T)
-        cov[k] /= n_effect[k]
-    cov = np.stack(cov)
+        # (9.26)
+        pi = np.zeros(n_components)
+        for k in range(n_components):
+            pi[k] = n_effect[k] / n_samples
 
-    return pi, mean, cov
+        # (9.24)
+        mean = np.zeros((n_components, n_features))
+        for k in range(n_components):
+            for n in range(n_samples):
+                mean[k] += gamma[n, k] * x[n]
+            mean[k] /= n_effect[k]
 
+        # (9.25)
+        cov = np.zeros((n_components, n_features, n_features))
+        for k in range(n_components):
+            for n in range(n_samples):
+                cov[k] += gamma[n, k] * np.dot(
+                    np.expand_dims(x[n] - mean[k], axis=-1),
+                    np.expand_dims(x[n] - mean[k], axis=-1).T)
+            cov[k] /= n_effect[k]
+        cov = np.stack(cov)
 
-def compute_log_likelihood(x, pi, mean, cov):
-    n_samples = len(x)
-    n_components = len(pi)
-
-    probs = np.zeros((n_samples, n_components))
-    for k in range(n_components):
-        probs[:, k] = pi[k] * multivariate_normal.pdf(x, mean[k], cov[k])
-
-    log_likelihood = np.sum(np.log(np.sum(probs, axis=1)))
-    return log_likelihood
-
-
-def log_likelihood_understandable(x, pi, mean, cov):
-    n_samples, _ = x.shape
-
-    log_likelihood = 0.0
-    for n in range(n_samples):
-        pdf_n = pi * np.array([multivariate_normal.pdf(x[n], mean_k, cov_k)
-                               for mean_k, cov_k in zip(mean, cov)])
-        log_likelihood += np.log(np.sum(pdf_n))
-    return log_likelihood
+        return pi, mean, cov
